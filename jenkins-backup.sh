be6fc45b57c0f5515577557b8bee5eca41f59a1c
Thiyagarajan787 <125461575+Thiyagarajan787@users.noreply.github.com>
2023-02-15 18:22:33 +0530

    Update jenkins-backup.sh

--- jenkins-backup.sh
+++ jenkins-backup.sh
@@ -90,7 +90,7 @@ function main() {
   cd "${TMP_DIR}"
   tar -czvf "${TMP_TAR_NAME}" "${ARC_NAME}/"*
   cd -
-  mv -f "${TMP_TAR_NAME}" "${DEST_FILE}"
+  cp -f "${TMP_TAR_NAME}" "${DEST_FILE}"
 
   cleanup
 
